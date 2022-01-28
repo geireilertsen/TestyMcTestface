fe0de37f9d3e21b6649698783521c1a96c6590ba
Geir Eilertsen <geir.eilertsen@netnordic.com>
2022-01-28 10:48:54 +0100

    5th commit in master

--- app/src/main/java/com/example/testymctestface/RegisterActivity.java
+++ app/src/main/java/com/example/testymctestface/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.testymctestface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void backButtonClicked(View view) {
+        Intent i = new Intent(view.getContext(), MainActivity.class);
+        startActivity(i);
     }
 }
